a47f1f6ac65b7dfe44d97a7a1cf1bb3041943538
Dyego Penha <dyegopenha@hotmail.com>
2021-12-03 15:17:13 -0300

    Conhecendo o helper select

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -3,6 +3,7 @@ class CoinsController < ApplicationController
 
   # fluxo mvc, antes de executar as acoes passadas como parametro ele vai setar a coin
   before_action :set_coin, only: %i[ show edit update destroy ]
+  before_action :set_mining_type_options, only: [:new, :create, :edit, :update]
 
   # GET /coins or /coins.json
   def index
@@ -60,6 +61,11 @@ class CoinsController < ApplicationController
   end
 
   private
+    def set_mining_type_options
+      @mining_type_options = MiningType.all.pluck(:description, :id)
+    end
+
+
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params[:id])
@@ -67,6 +73,6 @@ class CoinsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def coin_params
-      params.require(:coin).permit(:description, :acronym, :url_image)
+      params.require(:coin).permit(:description, :acronym, :url_image, :mining_type_id)
     end
 end
