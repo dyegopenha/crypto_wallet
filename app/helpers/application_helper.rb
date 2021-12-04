c71719542cb83234606ecdd8602b1c106306b7a7
Dyego Penha <dyegopenha@hotmail.com>
2021-12-04 02:48:26 -0300

    Ativando i18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+
+    def locale(locale)
+        I18n.locale == :en ? "Estados Unidos" : "Portugues do Brasil"
+    end
+
     def data_br(data_us)
         data_us.strftime("%d/%m/%Y")
     end
